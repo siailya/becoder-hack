f81d5fbb967828bb1cd60df78505fb633d2fe25f
Ilya <sia.ilya.dev@gmail.com>
2022-11-24 22:14:51 +0300

    Рефактор анализа

--- analyse/main.py
+++ analyse/main.py
@@ -3,24 +3,25 @@ import datetime
 
 url = "analyse_data_angular.json"
 
-with open(url, 'r')as file:
-    data=json.load(file)
-dic={}
-#print(data)
-for i in data:
-    author=i['author']
-    date=datetime.datetime.fromtimestamp(i['date']).strftime("%Y%m")
-    if date not in dic:
-        dic[date]={}
 
-    if author not in dic[date]:
-        dic[date][author]={}
+def parse_date(date):
+    with open(url, 'r') as file:
+        data = json.load(file)
+    dic = {}
 
-    if i['is_broken']:
-        if i['file'] not in dic[date][author]:
-            dic[date][author][i['file']]=0
+    for i in data:
+        author = i['author']
+        date = datetime.datetime.fromtimestamp(i['date']).strftime("%Y%m")
+        if date not in dic:
+            dic[date] = {}
 
-        dic[date][author][i['file']] +=1
+        if author not in dic[date]:
+            dic[date][author] = {}
 
-print(dic['201409'])
+        if i['is_broken']:
+            if i['file'] not in dic[date][author]:
+                dic[date][author][i['file']] = 0
 
+            dic[date][author][i['file']] += 1
+
+    return dic
