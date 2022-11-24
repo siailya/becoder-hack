8ce5f495f7043d01aad845975055607351544090
Aleksey Alasheev <aleksey.alasheev@bercut.com>
2022-11-24 23:13:55 +0300

    add sort_person_file

--- analyse/main.py
+++ analyse/main.py
@@ -1,10 +1,10 @@
-import json
 import datetime
+import json
 
 url = "analyse_data_angular.json"
 
 
-def parse_date(date):
+def parse_data():
     with open(url, 'r') as file:
         data = json.load(file)
     dic = {}
@@ -25,3 +25,20 @@ def parse_date(date):
             dic[date][author][i['file']] += 1
 
     return dic
+
+
+def sort_person_file(dic, author, file):
+    dic_author_file = {}
+    for i in dic:
+        dic_author_file[i] = 0
+        for a in dic[i]:
+            if a == author:
+                for f in dic[i][author]:
+                    if f == file:
+                        dic_author_file[i] += 1
+    return dic_author_file
+
+if __name__ == '__main__':
+    main_data = parse_data()
+    # print(main_data)
+    print(sort_person_file(main_data, "Alex Rickabaugh", "packages/compiler-cli/test/ngtsc/ngtsc_spec.ts"))
