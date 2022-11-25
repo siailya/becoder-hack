33d40421360751f8b43d3cb4b1dc0bb0477c9b83
Ilya <sia.ilya.dev@gmail.com>
2022-11-25 03:12:45 +0300

    Загрузка модели

--- catboost_model/execute.py
+++ catboost_model/execute.py
@@ -1,13 +1,20 @@
 import pandas as pd
 from catboost import CatBoostClassifier
+import time
+
 
 model = None
 
 
 def load_model():
     global model
+
+    print("Loading model...")
+    time_start = time.time()
     model = CatBoostClassifier()
     model.load_model("bccommit")
+
+    print("Model loaded in", time.time() - time_start, "seconds")
 
     return model
 
@@ -20,6 +27,7 @@ def model_predict(data):
 
 if __name__ == '__main__':
     load_model()
+
     test = {
         "file": ["abc.js"],
         "author": ["Ilya"],
@@ -33,4 +41,4 @@ if __name__ == '__main__':
 
     res = model.predict_proba(test_df)
 
-print(res)
+    print(res)
