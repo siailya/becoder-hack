e155e7a97ed82dfe654813e17cd0d67d3fd4f64a
Ilya <sia.ilya.dev@gmail.com>
2022-11-25 03:38:26 +0300

    Доделан /predict

--- catboost_model/execute.py
+++ catboost_model/execute.py
@@ -6,14 +6,14 @@ import time
 model = None
 
 
-def load_model():
+def load_model(model_path='catboost_model/bccommit'):
     global model
 
     print("Loading model...")
 
     time_start = time.time()
     model = CatBoostClassifier()
-    model.load_model("bccommit")
+    model.load_model(model_path)
 
     print("Model loaded in", time.time() - time_start, "seconds")
 
@@ -27,6 +27,8 @@ def model_predict(data):
 
 
 if __name__ == '__main__':
+    print("Testing model...")
+
     load_model()
 
     test = {
--- flask/hello.py
+++ flask/hello.py
@@ -1,12 +1,24 @@
-from flask import Flask, request
-from catboost_model.execute import load_model,model_predict
-model = load_model()
+import pandas as pd
+from flask import Flask, request, jsonify
+
+from catboost_model.execute import load_model, model_predict
+
+model = load_model(
+    model_path='../catboost_model/bccommit'
+)
 
 app = Flask(__name__)
 
+
 @app.post("/api/predict")
 def hello_world():
-    return model_predict(request.get_json())
+    df = pd.DataFrame.from_dict(request.get_json())
+
+    df["date"] = df['date'].astype(str)
+    res = model_predict(df)
+
+    return jsonify({"result": res[0][0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
