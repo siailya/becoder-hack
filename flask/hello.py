f30696995b589a6f16af4d1aeec9c300661e38a9
Ilya <sia.ilya.dev@gmail.com>
2022-11-25 04:26:35 +0300

    /predict возвращает чела для ревью

--- flask/hello.py
+++ flask/hello.py
@@ -1,11 +1,17 @@
-import pandas as pd
-from flask import Flask, request, jsonify
+import json
 
+import pandas as pd
+from flask import Flask, request, jsonify, Response
+
+from analyse.main import get_most_edit_person_for_file
 from catboost_model.execute import load_model, model_predict
 
 model = load_model(
     model_path='../catboost_model/bccommit'
 )
+
+with open("../analyse/analyse_data_angular.json", 'r') as read_file:
+    main_data = json.load(read_file)
 
 app = Flask(__name__)
 
@@ -16,8 +22,10 @@ def hello_world():
 
     df["date"] = df['date'].astype(str)
     res = model_predict(df)
-
-    return jsonify({"result": res[0][0]})
+    try:
+        return jsonify({"result": res[0][0], "person": get_most_edit_person_for_file(main_data, df["file"][0])})
+    except ValueError:
+        return Response("{'reason':'Не найден файл'}", status=502, mimetype='application/json')
 
 
 if __name__ == '__main__':
